041495abcae104ac0ea3aed88648488b91479414
Omar <omarsahmoud02@gmail.com>
2024-11-15 23:20:33 +0100

    edit-Omar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,44 @@
-import Exams from "@/sections/Examination/Exams";
 import styles from "./page.module.css";
-import EduTracks from "@/sections/EduTracks/EduTracks";
-import Contact from "@/sections/Contacts/Contact";
+import { Flex, Text, Box } from "@chakra-ui/react";
+import SimpleCard from "@/components/SimpleCard/SimpleCard";
+import ACAButton from "@/components/ACAButton/ACAButton";
+import eduLogo from "@/assets/eduLogo.png";
+import CustomIcon from "@/icons/Chalkboard";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Exams />
-        <EduTracks />
-        <Contact />
+        <Box display={"flex"} shadow= "0px 1px 20px 3px #00000040">
+        <SimpleCard text="قاموس المصطلحات التقنية" src={eduLogo} fontType="secondary">
+          <ACAButton
+            text="المسارات التعليمية"
+            icon={<CustomIcon />}
+            bg="cyan"
+            weight="900"
+            size="md"
+          />
+        </SimpleCard>
+        <SimpleCard text="قاموس المصطلحات التقنية" src={eduLogo} fontType="secondary">
+          <ACAButton
+            text="المسارات التعليمية"
+            icon={<CustomIcon />}
+            bg="cyan"
+            weight="900"
+            size="md"
+          />
+        </SimpleCard>
+        <SimpleCard text="قاموس المصطلحات التقنية" src={eduLogo} fontType="secondary">
+          <ACAButton
+            text="المسارات التعليمية"
+            icon={<CustomIcon />}
+            bg="cyan"
+            weight="900"
+            size="md"
+          />
+        </SimpleCard>
+
+        </Box>
       </main>
     </div>
   );
